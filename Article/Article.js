237d25694d1ad9fc34c54cf0b6b0a57b4ff57659
Noble Obioma <nobioma1@gmail.com>
2019-05-15 20:39:05 +0100

    STRETCH: Create Constructor Class that add new article from user

--- Article/Article.js
+++ Article/Article.js
@@ -4,12 +4,12 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
+    // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', this.expandArticle.bind(this))
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
     // create readButton
     this.readButton();
   }
@@ -30,17 +30,21 @@ class Article {
 
   readButton() {
     // Create READ Button
-    const heading = this.domElement.querySelector('h2')
-    const readBtn = document.createElement('button')
+    const heading = this.domElement.querySelector('h2');
+    const readBtn = document.createElement('button');
     readBtn.textContent = 'read';
     readBtn.setAttribute('class', 'readButton');
-    readBtn.addEventListener('click', () => this.read())
+    readBtn.addEventListener('click', () => this.read());
     heading.insertAdjacentElement('beforebegin', readBtn);
   }
 
   read() {
-    this.domElement.classList.add('remove')
-    TweenMax.to('.remove', 1.5, { left: -1000, display: 'none', ease: Bounce.easeOut })
+    this.domElement.classList.add('remove');
+    TweenMax.to('.remove', 1.5, {
+      left: -1000,
+      display: 'none',
+      ease: Bounce.easeOut
+    });
   }
 }
 
@@ -53,4 +57,69 @@ class Article {
 */
 
 let articles = document.querySelectorAll('.articles .article');
-articles.forEach(article => new Article(article))
+articles.forEach(article => new Article(article));
+
+class UserInput {
+  constructor() {
+    this.section = document.querySelector('.articles');
+    this.inputSection = document.querySelector('.text-input');
+    this.titleSection = this.inputSection.querySelector('#article-title');
+    this.articleSection = this.inputSection.querySelector('#article');
+    this.createArticle();
+  }
+
+  createArticle() {
+    let newTitle = this.titleSection.value;
+    let newArticleBody = this.articleSection.value;
+    const article = document.createElement('article');
+    article.setAttribute('class', 'article');
+    const h2 = document.createElement('h2');
+    const date = document.createElement('p');
+    date.setAttribute('class', 'date');
+    const pTag = document.createElement('p');
+    const span = document.createElement('span');
+    span.setAttribute('class', 'expandButton');
+
+    if (newTitle.length > 0 && newArticleBody.length > 0) {
+      let newDate = Date().split(' ');
+      h2.textContent = newTitle;
+      pTag.textContent = newArticleBody;
+      date.textContent = `${newDate[1]} ${newDate[2]}, ${newDate[3]}`;
+      article.appendChild(h2);
+      article.appendChild(date);
+      article.appendChild(pTag);
+      article.appendChild(span);
+
+      this.addArticle(article);
+    } else {
+      this.titleSection.placeholder = '...title cannot be empty';
+      this.articleSection.placeholder = '...article body cannot be empty';
+    }
+  }
+
+  addArticle(article) {
+    this.section.insertAdjacentElement('afterbegin', article);
+    new Article(article);
+    this.titleSection.value = '';
+    this.articleSection.value = '';
+  }
+}
+
+const addArticleButton = document
+  .querySelector('#add-articleBtn')
+  .addEventListener('click', () => new UserInput());
+
+
+const viewInputSectionBtn = document.querySelector('#open-inputBtn')
+const updateButton = () => {
+  if (viewInputSectionBtn.textContent === 'Add New Article') {
+    viewInputSectionBtn.textContent = 'Close'
+  } else {
+    TweenMax.to('.text-input', 0, { display: 'none' });
+    viewInputSectionBtn.textContent = 'Add New Article'
+  }
+}
+
+viewInputSectionBtn.addEventListener('click', () => {
+    TweenMax.to('.text-input', 0, { display: 'flex', onComplete: () => updateButton() })
+  });
